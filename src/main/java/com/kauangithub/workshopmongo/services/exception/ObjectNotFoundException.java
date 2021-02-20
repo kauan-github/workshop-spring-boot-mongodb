e66db65342271fc041c7a1eaf2ddd453b59b1200
kauan-github <kauanposowitch@gmail.com>
2021-02-19 23:28:53 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/kauangithub/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.kauangithub.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
