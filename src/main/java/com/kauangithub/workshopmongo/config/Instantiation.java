8d66606af065f4ac81b4c9e15edac73c85bc64c4
kauan-github <kauanposowitch@gmail.com>
2021-02-22 00:12:50 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/kauangithub/workshopmongo/config/Instantiation.java
+++ src/main/java/com/kauangithub/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.kauangithub.workshopmongo.domain.Post;
 import com.kauangithub.workshopmongo.domain.User;
 import com.kauangithub.workshopmongo.dto.AuthorDTO;
+import com.kauangithub.workshopmongo.dto.CommentDTO;
 import com.kauangithub.workshopmongo.repository.PostRepository;
 import com.kauangithub.workshopmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
